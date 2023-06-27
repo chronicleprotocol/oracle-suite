83b3e9e52453571d2730f78c220ad5075559431d
deep-quality-dev <hungrywarrior081@gmail.com>
2023-06-26 21:01:21 -0400

    Add Curve origin

--- config_next.hcl
+++ config_next.hcl
@@ -1,10 +1,64 @@
 # Test config for the gofernext and ghostnext apps. Not ready for production use.
 
+ethereum {
+  # Labels for generating random ethereum keys anew on every app boot.
+  # The labels are used to reference ethereum keys in other sections.
+  # (optional)
+  #
+  # If you want to use a specific key, you can set the CFG_ETH_FROM
+  # environment variable along with CFG_ETH_KEYS and CFG_ETH_PASS.
+  rand_keys = try(env.CFG_ETH_FROM, "") == "" ? ["default"] : []
+
+  dynamic "key" {
+    for_each = try(env.CFG_ETH_FROM, "") == "" ? [] : [1]
+    labels   = ["default"]
+    content {
+      address         = try(env.CFG_ETH_FROM, "")
+      keystore_path   = try(env.CFG_ETH_KEYS, "")
+      passphrase_file = try(env.CFG_ETH_PASS, "")
+    }
+  }
+
+  client "ethereum" {
+    rpc_urls = try(env.CFG_ETH_RPC_URLS == "" ? [] : split(",", env.CFG_ETH_RPC_URLS), [
+      "https://eth.public-rpc.com"
+    ])
+    chain_id     = tonumber(try(env.CFG_ETH_CHAIN_ID, "1"))
+    ethereum_key = "default"
+  }
+
+  client "arbitrum" {
+    rpc_urls = try(env.CFG_ETH_ARB_RPC_URLS == "" ? [] : split(",", env.CFG_ETH_ARB_RPC_URLS), [
+      "https://arbitrum.public-rpc.com"
+    ])
+    chain_id     = tonumber(try(env.CFG_ETH_ARB_CHAIN_ID, "42161"))
+    ethereum_key = "default"
+  }
+
+  client "optimism" {
+    rpc_urls = try(env.CFG_ETH_OPT_RPC_URLS == "" ? [] : split(",", env.CFG_ETH_OPT_RPC_URLS), [
+      "https://mainnet.optimism.io"
+    ])
+    chain_id     = tonumber(try(env.CFG_ETH_OPT_CHAIN_ID, "10"))
+    ethereum_key = "default"
+  }
+}
+
 gofernext {
   origin "coinbase" {
     type = "tick_generic_jq"
     url  = "https://api.pro.coinbase.com/products/$${ucbase}-$${ucquote}/ticker"
     jq   = "{price: .price, time: .time, volume: .volume}"
+  }
+
+  origin "curve" {
+    type = "curve"
+    contracts "ethereum" {
+      addresses = {
+        "RETH/WSTETH" = "0x447Ddd4960d9fdBF6af9a790560d0AF76795CB08",
+        "ETH/STETH"   = "0xDC24316b9AE028F1497c275EB9192a3Ea0f67022"
+      }
+    }
   }
 
   origin "binance" {
@@ -16,6 +70,14 @@ gofernext {
   data_model "BTC/USD" {
     origin "coinbase" { query = "BTC/USD" }
   }
+
+#  data_model "RETH/WSTETH" { # debug
+#    origin "curve" { query = "RETH/WSTETH" }
+#  }
+#
+#  data_model "ETH/STETH" { # debug
+#    origin "curve" { query = "ETH/STETH" }
+#  }
 }
 
 ghostnext {
---
+++ pkg/datapoint/origin/curve.go
@@ -0,0 +1,147 @@
+package origin
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"math/big"
+	"sort"
+	"time"
+
+	"github.com/defiweb/go-eth/abi"
+	"github.com/defiweb/go-eth/rpc"
+	"github.com/defiweb/go-eth/types"
+
+	"github.com/chronicleprotocol/oracle-suite/pkg/datapoint"
+	"github.com/chronicleprotocol/oracle-suite/pkg/datapoint/value"
+	"github.com/chronicleprotocol/oracle-suite/pkg/ethereum"
+	"github.com/chronicleprotocol/oracle-suite/pkg/log"
+	"github.com/chronicleprotocol/oracle-suite/pkg/log/null"
+	"github.com/chronicleprotocol/oracle-suite/pkg/util/bn"
+)
+
+//go:embed curve_abi.json
+var curvePoolABI []byte
+
+const CurveLoggerTag = "CURVE_ORIGIN"
+
+type CurveOptions struct {
+	Client            rpc.RPC
+	ContractAddresses ContractAddresses
+	Logger            log.Logger
+	Blocks            []int64
+}
+
+type Curve struct {
+	client                    rpc.RPC
+	contractAddresses         ContractAddresses
+	abi                       *abi.Contract
+	baseIndex, quoteIndex, dx *big.Int
+	blocks                    []int64
+	logger                    log.Logger
+}
+
+func NewCurve(opts CurveOptions) (*Curve, error) {
+	if opts.Client == nil {
+		return nil, fmt.Errorf("cannot nil ethereum client")
+	}
+	if opts.Logger == nil {
+		opts.Logger = null.New()
+	}
+
+	a, err := abi.ParseJSON(curvePoolABI)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Curve{
+		client:            opts.Client,
+		contractAddresses: opts.ContractAddresses,
+		abi:               a,
+		baseIndex:         big.NewInt(0),
+		quoteIndex:        big.NewInt(1),
+		dx:                new(big.Int).Mul(big.NewInt(1), new(big.Int).SetUint64(ether)),
+		blocks:            opts.Blocks,
+		logger:            opts.Logger.WithField("curve", CurveLoggerTag),
+	}, nil
+}
+
+func (c *Curve) FetchDataPoints(ctx context.Context, query []any) (map[any]datapoint.Point, error) {
+	pairs, ok := queryToPairs(query)
+	if !ok {
+		return nil, fmt.Errorf("invalid query type: %T, expected []Pair", query)
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].String() < pairs[j].String()
+	})
+
+	points := make(map[any]datapoint.Point)
+
+	block, err := c.client.BlockNumber(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot get block number, %w", err)
+	}
+
+	totals := make([]*big.Int, len(pairs))
+	var calls []types.Call
+	for i, pair := range pairs {
+		contract, inverted, err := c.contractAddresses.AddressByPair(pair)
+		if err != nil {
+			return nil, err
+		}
+		if inverted {
+			return nil, fmt.Errorf("cannnot use inverted pair to retrieve price: %s", pair.String())
+		}
+		var callData []byte
+		if !inverted {
+			callData, err = c.abi.Methods["get_dy"].EncodeArgs(c.baseIndex, c.quoteIndex, c.dx)
+		} else {
+			callData, err = c.abi.Methods["get_dy"].EncodeArgs(c.quoteIndex, c.baseIndex, c.dx)
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to pack contract args for getLatest (pair %s): %w", pair.String(), err)
+		}
+		calls = append(calls, types.Call{
+			To:    &contract,
+			Input: callData,
+		})
+		totals[i] = new(big.Int).SetInt64(0)
+	}
+
+	for _, blockDelta := range c.blocks {
+		resp, err := ethereum.MultiCall(ctx, c.client, calls, types.BlockNumberFromUint64(uint64(block.Int64()-blockDelta)))
+		if err != nil {
+			return nil, fmt.Errorf("failed multicall: %w", err)
+		}
+		if len(calls) != len(resp) {
+			return nil, fmt.Errorf("unexpected number of multicall results, expected %d, got %d", len(calls), len(resp))
+		}
+		if len(resp) != len(pairs) {
+			return nil, fmt.Errorf("unexpected number of multicall results with pairs, expected %d, got %d", len(resp), len(pairs))
+		}
+
+		for i, _ := range pairs {
+			price := new(big.Int).SetBytes(resp[0][0:32])
+			totals[i] = totals[i].Add(totals[i], price)
+		}
+	}
+
+	for i, pair := range pairs {
+		avgPrice := new(big.Float).Quo(new(big.Float).SetInt(totals[i]), new(big.Float).SetUint64(ether))
+		avgPrice = avgPrice.Quo(avgPrice, new(big.Float).SetUint64(uint64(len(c.blocks))))
+
+		tick := value.Tick{
+			Pair:      pair,
+			Price:     bn.Float(avgPrice),
+			Volume24h: nil,
+		}
+		points[pair] = datapoint.Point{
+			Value: tick,
+			Time:  time.Now(),
+		}
+	}
+
+	return points, nil
+}
