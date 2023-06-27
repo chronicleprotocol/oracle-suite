ad214c15d21ac1dbc0974d2378bba50547375d1f
deep-quality-dev <hungrywarrior081@gmail.com>
2023-06-27 19:57:09 -0400

    Remove nolint comment

--- pkg/datapoint/origin/balancer_v2.go
+++ pkg/datapoint/origin/balancer_v2.go
@@ -66,7 +66,6 @@ func NewBalancerV2(opts BalancerV2Options) (*BalancerV2, error) {
 	}, nil
 }
 
-//nolint:funlen
 func (b *BalancerV2) FetchDataPoints(ctx context.Context, query []any) (map[any]datapoint.Point, error) {
 	pairs, ok := queryToPairs(query)
 	if !ok {
