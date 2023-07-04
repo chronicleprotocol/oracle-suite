3b4602f0211b51d66701511b6bcf7343fde72978
deep-quality-dev <hungrywarrior081@gmail.com>
2023-07-04 14:43:21 -0400

    Fix individual error per each point

--- pkg/datapoint/origin/rocketpool.go
+++ pkg/datapoint/origin/rocketpool.go
@@ -89,7 +89,8 @@ func (r *RocketPool) FetchDataPoints(ctx context.Context, query []any) (map[any]
 	for i, pair := range pairs {
 		contract, inverted, err := r.contractAddresses.AddressByPair(pair)
 		if err != nil {
-			return nil, err
+			points[pair] = datapoint.Point{Error: err}
+			continue
 		}
 
 		var callData []byte
@@ -99,7 +100,12 @@ func (r *RocketPool) FetchDataPoints(ctx context.Context, query []any) (map[any]
 			callData, err = r.abi.Methods["getRethValue"].EncodeArgs(big.NewInt(0).SetUint64(ether))
 		}
 		if err != nil {
-			return nil, fmt.Errorf("failed to get contract args for pair: %s: %w", pair.String(), err)
+			points[pair] = datapoint.Point{Error: fmt.Errorf(
+				"failed to get contract args for pair: %s: %w",
+				pair.String(),
+				err,
+			)}
+			continue
 		}
 		calls = append(calls, types.Call{
 			To:    &contract,
@@ -108,27 +114,33 @@ func (r *RocketPool) FetchDataPoints(ctx context.Context, query []any) (map[any]
 		totals[i] = new(big.Int).SetInt64(0)
 	}
 
-	for _, blockDelta := range r.blocks {
-		resp, err := ethereum.MultiCall(ctx, r.client, calls, types.BlockNumberFromUint64(uint64(block.Int64()-blockDelta)))
-		if err != nil {
-			return nil, fmt.Errorf("failed multicall: %w", err)
-		}
-		if len(calls) != len(resp) {
-			return nil, fmt.Errorf("unexpected number of multicall results, expected %d, got %d",
-				len(calls), len(resp))
-		}
-		if len(resp) != len(pairs) {
-			return nil, fmt.Errorf("unexpected number of multicall results with pairs, expected %d, got %d",
-				len(resp), len(pairs))
-		}
+	if len(calls) > 0 {
+		for _, blockDelta := range r.blocks {
+			resp, err := ethereum.MultiCall(ctx, r.client, calls, types.BlockNumberFromUint64(uint64(block.Int64()-blockDelta)))
+			if err != nil {
+				return nil, fmt.Errorf("failed multicall: %w", err)
+			}
+			if len(calls) != len(resp) {
+				return nil, fmt.Errorf("unexpected number of multicall results, expected %d, got %d",
+					len(calls), len(resp))
+			}
+			if len(resp) != len(pairs) {
+				return nil, fmt.Errorf("unexpected number of multicall results with pairs, expected %d, got %d",
+					len(resp), len(pairs))
+			}
 
-		for i := range pairs {
-			price := new(big.Int).SetBytes(resp[i][0:32])
-			totals[i] = totals[i].Add(totals[i], price)
+			for i := range pairs {
+				price := new(big.Int).SetBytes(resp[i][0:32])
+				totals[i] = totals[i].Add(totals[i], price)
+			}
 		}
 	}
 
 	for i, pair := range pairs {
+		if points[pair].Error != nil {
+			continue
+		}
+
 		avgPrice := new(big.Float).Quo(new(big.Float).SetInt(totals[i]), new(big.Float).SetUint64(ether))
 		avgPrice = avgPrice.Quo(avgPrice, new(big.Float).SetUint64(uint64(len(r.blocks))))
 
@@ -143,5 +155,8 @@ func (r *RocketPool) FetchDataPoints(ctx context.Context, query []any) (map[any]
 		}
 	}
 
+	if len(pairs) == 1 && points[pairs[0]].Error != nil {
+		return points, points[pairs[0]].Error
+	}
 	return points, nil
 }
