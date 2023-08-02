b282a5286ee08868dfe8d6f5b57f9e3bf3ec43b0
Paweł Zaremba <pawzar@gmail.com>
2023-08-02 14:30:43 +0200

    Fix test

--- e2e/ghost_test.go
+++ e2e/ghost_test.go
@@ -1,3 +1,18 @@
+//  Copyright (C) 2021-2023 Chronicle Labs, Inc.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package e2e
 
 import (
@@ -52,7 +67,7 @@ func Test_Ghost_ValidPrice(t *testing.T) {
 	require.NoError(t, err)
 
 	spireCmd := command(ctx, "..", nil, "./spire", "agent", "-c", "./e2e/testdata/config/spire.hcl", "-v", "debug")
-	ghostCmd := command(ctx, "..", nil, "./ghost", "run", "-c", "./e2e/testdata/config/ghost.hcl", "--gofer.norpc", "-v", "debug")
+	ghostCmd := command(ctx, "..", nil, "./ghost", "run", "-c", "./e2e/testdata/config/ghost.hcl", "-v", "debug")
 	defer func() {
 		ctxCancel()
 		_ = spireCmd.Wait()
@@ -108,7 +123,7 @@ func Test_Ghost_InvalidPrice(t *testing.T) {
 	require.NoError(t, err)
 
 	spireCmd := command(ctx, "..", nil, "./spire", "agent", "-c", "./e2e/testdata/config/spire.hcl", "-v", "debug")
-	ghostCmd := command(ctx, "..", nil, "./ghost", "run", "-c", "./e2e/testdata/config/ghost.hcl", "--gofer.norpc", "-v", "debug")
+	ghostCmd := command(ctx, "..", nil, "./ghost", "run", "-c", "./e2e/testdata/config/ghost.hcl", "-v", "debug")
 	defer func() {
 		ctxCancel()
 		_ = spireCmd.Wait()
