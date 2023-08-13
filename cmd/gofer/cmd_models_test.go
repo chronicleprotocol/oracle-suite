6f9cf4bf845be16713a623ba4df8cb0e0cfd14ff
Hungry Warrior <hungrywarrior081@gmail.com>
2023-08-13 17:05:04 -0400

    Fix unit test regardless capturing output

--- cmd/gofer/cmd_models_test.go
+++ cmd/gofer/cmd_models_test.go
@@ -16,8 +16,11 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"log"
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -57,36 +60,50 @@ XTZ/USD
 YFI/USD
 `[1:]
 
-func TestNewModelsCmd_List(t *testing.T) {
+func captureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
 	stdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		log.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		io.Copy(&buf, reader)
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
+}
 
-	os.Args = []string{"gofer", "-c", "../../config.hcl", "models"}
-	main()
-
-	os.Stdout = stdout
-
-	_ = w.Close()
-	out, _ := io.ReadAll(r)
-
-	assert.Equal(t, modelList, string(out))
+func TestNewModelsCmd_List(t *testing.T) {
+	out := captureOutput(func() {
+		os.Args = []string{"gofer", "-c", "../../config.hcl", "models"}
+		main()
+	})
+	assert.Equal(t, modelList, out)
 }
 
 func TestNewModelsCmd(t *testing.T) {
-	stdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	os.Args = []string{"gofer", "-c", "../../config.hcl", "models", "-o", "trace", "--no-color"}
-	main()
-
-	os.Stdout = stdout
-
-	_ = w.Close()
-	out, _ := io.ReadAll(r)
-
-	assert.Equal(t, completeDataModels, string(out))
+	out := captureOutput(func() {
+		os.Args = []string{"gofer", "-c", "../../config.hcl", "models", "-o", "trace", "--no-color"}
+		main()
+	})
+	assert.Equal(t, completeDataModels, out)
 }
 
 var completeDataModels = `
@@ -1857,4 +1874,5 @@ Model for YFI/USD:
                ├──origin(expiry_threshold:5m0s, freshness_threshold:1m0s, origin:coinbase, query:ETH/USD)
                ├──origin(expiry_threshold:5m0s, freshness_threshold:1m0s, origin:gemini, query:ETH/USD)
                └──origin(expiry_threshold:5m0s, freshness_threshold:1m0s, origin:kraken, query:ETH/USD)
+
 `[1:]
