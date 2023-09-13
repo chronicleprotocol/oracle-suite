85c8563ff497f70f10529bb70b2cfa1b3de93b6d
Paweł Zaremba <pawzar@gmail.com>
2023-09-13 14:53:15 +0200

    Enable Hole Punching

--- pkg/transport/libp2p/internal/node.go
+++ pkg/transport/libp2p/internal/node.go
@@ -126,6 +126,7 @@ func (n *Node) Start(ctx context.Context) error {
 	n.host, err = libp2p.New(append([]libp2p.Option{
 		libp2p.ForceReachabilityPublic(),
 		libp2p.EnableRelayService(),
+		libp2p.EnableHolePunching(),
 		libp2p.EnableNATService(),
 		libp2p.Peerstore(n.peerstore),
 		libp2p.ConnectionGater(n.connGaterSet),
