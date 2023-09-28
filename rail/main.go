63c6a83438d298b0eb6d72d71e833c51679b0959
Paweł Zaremba <pawzar@gmail.com>
2023-09-28 12:17:17 +0200

    Add env vars

--- rail/main.go
+++ rail/main.go
@@ -36,15 +36,25 @@ import (
 
 var log = logging.Logger("rail")
 
-func main() {
-	logging.SetAllLoggers(logging.LevelInfo)
-	if err := logging.SetLogLevel("rail", "debug"); err != nil {
-		log.Fatal(err)
+func env(key, def string) string {
+	v, ok := os.LookupEnv(key)
+	if !ok {
+		return def
 	}
-	if err := logging.SetLogLevel("rail/service", "info"); err != nil {
-		log.Fatal(err)
-	}
+	return v
+}
 
+func main() {
+	// logging.SetAllLoggers(logging.LevelInfo)
+	logLevel, ok := os.LookupEnv("CFG_LOG_LEVEL")
+	if ok {
+		if err := logging.SetLogLevel("rail", logLevel); err != nil {
+			log.Fatal(err)
+		}
+		if err := logging.SetLogLevel("rail/service", logLevel); err != nil {
+			log.Fatal(err)
+		}
+	}
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer stop()
 
